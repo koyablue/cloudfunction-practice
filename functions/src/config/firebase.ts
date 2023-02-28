9c62a676ab90db4f4f99ae0738ad1c9891820a20
koyaaoyama <aoyama.ko0920@gmail.com>
2023-02-28 15:15:20 +0900

    update gitignore

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -1,10 +1,14 @@
 import * as admin from 'firebase-admin'
 
+import { config } from './firebaseConfigJson'
+
+// tried to use env values but didn't work well somehow
+// it seemed that env values are empty while deploying
 admin.initializeApp({
   credential: admin.credential.cert({
-    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    projectId: process.env.PROJECT_ID,
-    clientEmail: process.env.CLIENT_EMAIL,
+    projectId: config.project_id,
+    clientEmail: config.client_email,
+    privateKey: config.private_key.replace(/\\n/gm, "\n"),
   }),
   // https://<app-name>.firebaseio.com
   databaseURL: 'https://rest-api-journal.firebaseio.com',
