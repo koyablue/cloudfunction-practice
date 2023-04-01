f53e2ac70e02d5a9aca9f3034b0b9e9f1d511bea
koyaaoyama <aoyama.ko0920@gmail.com>
2023-04-01 09:59:23 -0700

    use envfile for firebase credentials

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -1,14 +1,20 @@
 import * as admin from 'firebase-admin'
+import * as dotenv from 'dotenv'
 
-import { config } from './firebaseConfigJson'
+// import { config } from './firebaseConfigJson'
+
+dotenv.config()
 
 // tried to use env values but didn't work well somehow
 // it seemed that env values are empty while deploying
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: config.project_id,
-    clientEmail: config.client_email,
-    privateKey: config.private_key.replace(/\\n/gm, "\n"),
+    // projectId: config.project_id,
+    // clientEmail: config.client_email,
+    // privateKey: config.private_key.replace(/\\n/gm, "\n"),
+    projectId: process.env.PROJECT_ID,
+    clientEmail: process.env.CLIENT_EMAIL,
+    privateKey: process.env.SERVICE_ACCOUNT_PRIVATE_KEY?.replace(/\\n/gm, "\n"),
   }),
   // https://<app-name>.firebaseio.com
   databaseURL: 'https://rest-api-journal.firebaseio.com',
