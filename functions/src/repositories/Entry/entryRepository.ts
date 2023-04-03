7a6a2bd288a3d2d44b728f870ce123d07bbb8980
koyaaoyama <aoyama.ko0920@gmail.com>
2023-04-02 19:23:10 -0700

    implement createEntry in entryRepository

--- functions/src/repositories/Entry/entryRepository.ts
+++ functions/src/repositories/Entry/entryRepository.ts
@@ -1,1 +1,30 @@
-const createEntry = () => {}
+import { CreateEntitySuccessResponse, CreateEntryRequest } from './types'
+import { Result, newFailure, newSuccess } from '../../models/Result'
+import { ErrorResponse } from '../../models/ErrorResponse'
+import { db } from '../../config/firebase'
+import { Entry } from '../../models/Entry'
+import { entryConverter } from '../../infrastructures/firebase/converters/entryConverter'
+
+const entryRef = db.collection('entity').withConverter<Entry>(entryConverter)
+
+const createEntry = async ({ title, text }: CreateEntryRequest): Promise<Result<Entry, unknown>>  => {
+  try {
+    const entry = entryRef.doc()
+
+    const entryObject = {
+      id: entry.id,
+      title,
+      text
+    }
+
+    await entry.set(entryObject)
+
+    return newSuccess(entryObject)
+  } catch(error) {
+    return newFailure(error)
+  }
+}
+
+export {
+  createEntry
+}
